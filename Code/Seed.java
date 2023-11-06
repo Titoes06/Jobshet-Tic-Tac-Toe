5a9cf4757b1378995f8060f77495307cf5544f89
Titoes06 <148724883+Titoes06@users.noreply.github.com>
2023-11-06 09:57:49 +0700

    Delete Code directory

--- Code/Seed.java
+++
@@ -1,7 +0,0 @@
-
-
-/** * Enumeration for the seeds and cell contents */ public enum Seed { 
-// to save as "Seed.java" 
-EMPTY, CROSS, NOUGHT 
-}
-
--- Code/State.java
+++
@@ -1,5 +0,0 @@
-
-/** * Enumeration for the various states of the game */ public enum 
-State { // to save as "GameState.java" 
-PLAYING, DRAW, CROSS_WON, NOUGHT_WON 
-}
